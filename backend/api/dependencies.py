0210a605107d4268b3a238c05c794a19042d8e8a
unknown <maks-kandikov@yandex.ru>
2024-11-15 16:35:34 +0300

    Будущие зависимости, которые будут в /backend/api/...

---
+++ backend/api/dependencies.py
@@ -0,0 +1,12 @@
+from fastapi import Header, HTTPException, Depends, status
+
+
+async def get_api_key(api_key: str = Header(...)):
+    # Поменять эту заглушку, когда сделаю бд
+    valid_api_keys = ["test", "user", "admin"]
+    if api_key not in valid_api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key"
+        )
+    return api_key
